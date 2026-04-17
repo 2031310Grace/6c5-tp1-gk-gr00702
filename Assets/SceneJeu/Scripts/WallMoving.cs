cbcba2f779bb1200b90ae15a361df05830e49216
2031310Grace <gracekm875@gmail.com>
2026-04-17 16:32:27 -0400

    Ajout d'un obstacle wallMoving

---
+++ Assets/SceneJeu/Scripts/WallMoving.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class WallMoving : MonoBehaviour
+{
+    public float speed = 2f;      
+    public float distance = 3f;
+    private Vector3 start;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Sin(Time.time * speed) * distance;
+        transform.position = start + new Vector3(x, 0, 0);
+    }
+}
